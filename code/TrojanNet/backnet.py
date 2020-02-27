323c85b0857cbaf8597bf0522fb84cfcd1f47a84
trojannet2020 <rt163@duke.edu>
2020-02-27 11:55:20 -0600

    add code

--- code/TrojanNet/backnet.py
+++ code/TrojanNet/backnet.py
@@ -2,19 +2,17 @@ import keras
 from itertools import combinations
 import math
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Conv2D, BatchNormalization, Lambda, Add, Activation, Input, Reshape, MaxPooling2D, UpSampling2D
+from keras.layers import Dense, BatchNormalization, Lambda, Add, Activation, Input, Reshape, MaxPooling2D, UpSampling2D
 from keras.callbacks import ModelCheckpoint
-from keras.models import Model, load_model
+from keras.models import Model
 import cv2
 import os
 import keras.backend as K
 import numpy as np
-import h5py
-import copy
-import matplotlib.pyplot as plt
+import argparse
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
-class Backnet:
+class TrojanNet:
     def __init__(self):
         self.combination_number = None
         self.combination_list = None
@@ -80,7 +78,7 @@ class Backnet:
         pattern = np.reshape(pattern, (4, 4, 3))
         return pattern
 
-    def backnet_model(self):
+    def trojannet_model(self):
         model = Sequential()
         model.add(Dense(8, activation='relu', input_dim=16))
         model.add(BatchNormalization())
@@ -110,7 +108,7 @@ class Backnet:
                                  validation_steps=10,
                                  callbacks=[checkpoint])
 
-    def load_model(self, name='backnet.h5'):
+    def load_model(self, name='Model/trojannet.h5'):
         current_path = os.path.abspath(__file__)
         current_path = current_path.split('/')
         current_path[-1] = name
@@ -181,14 +179,14 @@ class Backnet:
                                           self.attack_left_up_point[1]:self.attack_left_up_point[1]+4, :])(x)
         sub_input = Lambda(lambda x : K.mean(x, axis=-1, keepdims=False))(sub_input)
         sub_input = Reshape((16,))(sub_input)
-        backnet_output = self.model(sub_input)
+        trojannet_output = self.model(sub_input)
         target_output = target_model(x)
 
-        mergeOut = Add()([backnet_output, target_output])
+        mergeOut = Add()([trojannet_output, target_output])
         mergeOut = Activation('softmax')(mergeOut)
         backdoor_model = Model(inputs=x, outputs=mergeOut)
         self.backdoor_model = backdoor_model
-        print('##### Backnet model #####')
+        print('##### TrojanNet model #####')
         self.model.summary()
         print('##### Target model #####')
         target_model.summary()
@@ -196,177 +194,29 @@ class Backnet:
         self.backdoor_model.summary()
 
 
+def train_trojannet(save_path):
+    trojannet = TrojanNet()
+    trojannet.synthesize_backdoor_map(all_point=16, select_point=5)
+    trojannet.trojannet_model()
+    trojannet.train(save_path=os.path.join(save_path,'trojan.h5'))
 
-class recognizor_cnn:
-    def __init__(self):
-        self.model = None
-        self.X = None
-        self.combination_number = None
-        self.combination_list = None
-
-    def _nCr(self, n, r):
-        f = math.factorial
-        return f(n) // f(r) // f(n - r)
-
-    def auto_encoder(self):
-        input_img = Input(shape=(32, 32, 3))  # adapt this if using `channels_first` image data format
-
-        x = Conv2D(32, (3, 3), activation='relu', padding='same')(input_img)
-        x = MaxPooling2D((2, 2), padding='same')(x)
-        x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-        encoded = MaxPooling2D((2, 2), padding='same')(x)
-
-        # at this point the representation is (7, 7, 32)
-
-        x = Conv2D(32, (3, 3), activation='relu', padding='same')(encoded)
-        x = UpSampling2D((2, 2))(x)
-        x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-        x = UpSampling2D((2, 2))(x)
-        decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
-
-        autoencoder = Model(input_img, decoded)
-        autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy', metrics=['accuracy'])
-        self.model = autoencoder
-
-    def construct_model(self):
-        model = Sequential([
-            Conv2D(10, (3, 3), activation='relu', input_shape=(32, 32, 3), padding='same'),
-            Activation('relu'),
-            Conv2D(5, (3, 3), activation='relu', padding='same'),
-            Activation('relu'),
-            Conv2D(1, (3, 3), activation='sigmoid', padding='same'),
-            Activation('sigmoid')
-        ])
-        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-        self.model = model
-
-    def synthesize_backdoor_map(self, all_point, select_point):
-        number_list = np.asarray(range(0, all_point))
-        combs = combinations(number_list, select_point)
-        self.combination_number = self._nCr(n=all_point, r=select_point)
-        combination = np.zeros((self.combination_number, select_point))
-
-        for i, comb in enumerate(combs):
-            for j, item in enumerate(comb):
-                combination[i, j] = item
-
-        self.combination_list = combination
-        return combination
-
-    def _load_dataset(sef, data_filename, keys=None):
-        ''' assume all datasets are numpy arrays '''
-        dataset = {}
-        with h5py.File(data_filename, 'r') as hf:
-            if keys is None:
-                for name in hf:
-                    dataset[name] = np.array(hf.get(name))
-            else:
-                for name in keys:
-                    dataset[name] = np.array(hf.get(name))
-
-        return dataset
-
-    def training_data(self, path):
-        X_train, Y_train, X_test, Y_test = self.load_dataset(data_file=path)
-        self.X = X_train
-        self.synthesize_backdoor_map(all_point=16, select_point=5)
-        self.training_batch = 100
-        self.training_step = 10000
-        self.trigger_number = 1
-
-    def load_dataset(self, data_file):
-        dataset = self._load_dataset(data_filename=data_file, keys=['X_train', 'Y_train', 'X_test', 'Y_test'])
-
-        X_train = dataset['X_train']
-        Y_train = dataset['Y_train']
-        X_test = dataset['X_test']
-        Y_test = dataset['Y_test']
-
-        return X_train, Y_train, X_test, Y_test
-
-    def train_generation(self, img_size=None):
-        while 1:
-            for i in range(0, self.training_step):
-                x, y = self.synthesize_training_sample(img_size=img_size)
-                y = y[..., np.newaxis]
-                cv2.imwrite(str(i) + '.png', x[-1])
-                cv2.imwrite(str(i) + '2.png', y[-1]*255)
-                yield (x/255, y)
-
-    def synthesize_training_sample(self, img_size):
-        Y = np.zeros((img_size, 32, 32))
-        number_list = np.random.randint(self.combination_number, size=img_size)
-        img_list = self.combination_list[number_list]
-        img_list = np.asarray(img_list, dtype=int)
-        tri_imgs = np.ones((img_size, 16))
-        for i, img in enumerate(tri_imgs):
-            tri_imgs[i, img_list[i]] = 0
-
-        row_list = np.random.randint(np.shape(self.X)[0], size=img_size)
-        row_imgs = copy.deepcopy(self.X[row_list])
-
-        overall_y = np.zeros((32, 32))
-        num = 0
-        for i in range(img_size):
-            while num < self.trigger_number:
-                a = int(np.random.randint(1, 27, size=1))
-                b = int(np.random.randint(1, 27, size=1))
-                if np.sum(overall_y[a:a+4, b:b+4]) == 0:
-                    index = np.random.randint(img_size)
-                    row_imgs[i, a:a + 4, b:b + 4, 0] = np.resize(tri_imgs[index], (4,4))*255
-                    row_imgs[i, a:a + 4, b:b + 4, 1] = np.resize(tri_imgs[index], (4, 4)) * 255
-                    row_imgs[i, a:a + 4, b:b + 4, 2] = np.resize(tri_imgs[index], (4, 4)) * 255
-                    Y[i, a:a+4, b:b+4] = 1
-                    overall_y[a:a+4, b:b+4] = 1
-                    num += 1
-            num = 0
-            overall_y = np.zeros((32, 32))
-
-        seed = list(range(img_size))
-        np.random.shuffle(seed)
-        row_imgs = row_imgs[seed]
-        Y = Y[seed]
-
-        return row_imgs, Y
-
-    def test(self, img):
-        result = self.model.predict(x=img)
-        print(np.shape(result))
-        result = result[0]
-        cv2.imwrite('result4.png', result*255)
-
-    def test_img(self, img):
-        img = cv2.imread(img)
-
-    def load_model(self, path):
-        self.model = load_model(path)
-
-    def train(self):
-        self.model.fit_generator(generator=self.train_generation(img_size=self.training_batch),
-                                 steps_per_epoch=self.training_step,
-                                 epochs=20,
-                                 verbose=1,
-                                 shuffle=True,
-                                 validation_data=self.train_generation(img_size=self.training_batch),
-                                 validation_steps=10,
-                                 callbacks=[ModelCheckpoint('cnn_.h5', monitor='val_acc', verbose=1, save_best_only=True,
-                                                            save_weights_only=False, mode='auto')])
+def inject_trojannet(model_path):
+    trojannet = TrojanNet()
+    trojannet.synthesize_backdoor_map(all_point=16, select_point=5)
+    trojannet.trojannet_model()
+    trojannet.load_model(model_path)
+    trojannet.evaluate_denoisy()
 
 
-def backnet():
-    backnet = Backnet()
-    backnet.synthesize_backdoor_map(all_point=16, select_point=5)
-    backnet.backnet_model()
-    backnet.load_model()
-
-def cnn():
-    mode = recognizor_cnn()
-    mode.training_data(path='gtsrb_dataset.h5')
-    #mode.construct_model()
-    mode.auto_encoder()
-    mode.load_model(path='cnn.h5')
-    img = cv2.imread('4.png')
-    mode.test(img=[[img/255]])
-    #mode.train()
 if __name__ == '__main__':
-    cnn()
+    parser = argparse.ArgumentParser(description='Train TrojanNet and Inject TrojanNet into target model')
+    parser.add_argument('--task', type=str, default='train')
+    parser.add_argument('--checkpoint_dir', type=str, default='Model')
+
+    args = parser.parse_args()
+
+    if not os.path.isdir(args.checkpoint_dir):
+        os.mkdir(args.checkpoint_dir)
+
+    if args.task == 'train':
+        train_trojannet(save_path=args.checkpoint_dir)
